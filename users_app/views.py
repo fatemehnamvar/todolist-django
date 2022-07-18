2b4ccb29a68034159e7111ceed6d24e17e901e9f
fatemehnamvar <fatemehnamvar1424@gmail.com>
2022-07-18 19:40:12 +0430

    edit homepage and add users app for login

---
+++ users_app/views.py
@@ -0,0 +1,9 @@
+import imp
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+def register(request):
+    return HttpResponse("this is login page")
