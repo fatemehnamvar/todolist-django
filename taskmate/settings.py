3c926c2a4391a3be4f306b116e2610ec52e12e8f
fatemehnamvar <fatemehnamvar1424@gmail.com>
2022-07-19 00:42:39 +0430

    add regiteration option

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'todolist_app',
-    'users_app'
+    'users_app',
+    'crispy_forms'
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,4 @@ STATICFILES_DIRS = [(os.path.join(BASE_DIR, 'static'))]
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CRISPY_TELPLATE_PACK = 'bootstratp5'
--- users_app/views.py
+++ users_app/views.py
@@ -1,9 +1,21 @@
+from email import message
 import imp
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from .forms import CustomRegisterForm
 
 # Create your views here.
 
 
 def register(request):
-    return HttpResponse("this is login page")
+    if request.method == "POST":
+        register_form = CustomRegisterForm(request.POST)
+        if register_form.is_valid():
+            register_form.save()
+            messages.success(request, ("New user accout created."))
+            return redirect('register')
+    else:
+        register_form = CustomRegisterForm()
+    return render(request, "register.html", {"register_form": register_form})
